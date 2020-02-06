9910c0d44fab83ca99756fe65e12016e98593258
luckyleonard <dzhu31@hotmail.com>
2020-02-06 18:35:08 +1100

    refactor with seprate document

--- App/index.js
+++ App/index.js
@@ -1,38 +1,10 @@
 const Koa = require('koa');
-const Router = require('koa-router');
 const app = new Koa();
-const router = new Router();
-const userRouter = new Router({ prefix: '/users' });
 const bodyparser = require('koa-bodyparser');
-
-router.get('/', ctx => {
-  ctx.body = 'this is homepage';
-});
-
-userRouter.get('/', ctx => {
-  ctx.body = 'this is user list';
-});
-
-userRouter.post('/', ctx => {
-  ctx.body = 'create a user';
-});
-
-userRouter.get('/:id', ctx => {
-  ctx.body = `this user is ${ctx.params.id}`;
-});
-
-userRouter.put('/:id', ctx => {
-  ctx.body = `this user is ${ctx.params.id}`;
-});
-
-userRouter.delete('/:id', ctx => {
-  ctx.status = 204;
-});
+const routing = require('./routes');
 
 app.use(bodyparser());
-app.use(router.routes());
-app.use(userRouter.routes());
-app.use(userRouter.allowedMethods());
+routing(app);
 
 app.listen(3000, () => {
   console.log(`Server running at http://localhost:3000`);
