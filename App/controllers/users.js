ecbaf26965d9ed72e3f4277a0d49d1b2b5ba8363
luckyleonard <dzhu31@hotmail.com>
2020-02-08 00:55:13 +1100

    Achieve params verify with Koa-parameter

--- App/controllers/users.js
+++ App/controllers/users.js
@@ -11,14 +11,23 @@ class userCtl {
     ctx.body = db[ctx.params.id * 1];
   }
   createUser(ctx) {
+    ctx.verifyParams({
+      name: { type: 'string', required: true }
+    });
     db.push(ctx.request.body);
     ctx.body = ctx.request.body;
   }
   updateUser(ctx) {
+    ctx.verifyParams({
+      name: { type: 'string', required: true }
+    });
     db[ctx.params.id * 1] = ctx.request.body;
     ctx.body = ctx.request.body;
   }
   deleteUser(ctx) {
+    if (ctx.params.id * 1 >= db.length) {
+      ctx.status(412, 'ID is out of range');
+    }
     db.splice(ctx.params.id * 1, 1);
     ctx.status = 204;
   }
--- App/index.js
+++ App/index.js
@@ -2,19 +2,30 @@ const Koa = require('koa');
 const app = new Koa();
 const bodyparser = require('koa-bodyparser');
 const routing = require('./routes');
+const error = require('koa-json-error');
+const parameter = require('koa-parameter');
 
-app.use(async (ctx, next) => {
-  try {
-    await next();
-  } catch (err) {
-    ctx.status = err.status || err.statusCode || 500;
-    ctx.body = {
-      message: err.message //res a JSON object
-    };
-  }
-});
+// app.use(async (ctx, next) => {
+//   try {
+//     await next();
+//   } catch (err) {
+//     ctx.status = err.status || err.statusCode || 500;
+//     ctx.body = {
+//       message: err.message //res a JSON object
+//     };
+//   }
+// }); //error handler with own design
+
+app.use(
+  error({
+    postFormat: (e, { stack, ...rest }) =>
+      process.env.NODE_ENV === 'production' ? rest : { stack, ...rest }
+  })
+);
 
 app.use(bodyparser());
+
+app.use(parameter(app));
 
 routing(app);
 
