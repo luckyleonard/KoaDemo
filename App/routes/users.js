24ed29583e25a9934aabc54b3e3a3154a232124f
luckyleonard <dzhu31@hotmail.com>
2020-02-19 01:44:11 +1100

    ✨ feat(user): Achieve user login authorization

--- App/routes/users.js
+++ App/routes/users.js
@@ -1,3 +1,5 @@
+const jsonwebtoken = require('jsonwebtoken');
+const { secret } = require('../config');
 const Router = require('koa-router');
 const router = new Router({ prefix: '/users' });
 
@@ -10,15 +12,27 @@ const {
   login
 } = require('../controllers/users');
 
+const auth = async (ctx, next) => {
+  const { authorization = '' } = ctx.request.header; //短路语法，如果authorization为空则赋值空字符串
+  const token = authorization.replace('Bearer ', ''); //替换从请求头接收的authorization字符串里的‘Bearer ’为空
+  try {
+    const user = jsonwebtoken.verify(token, secret);
+    ctx.state.user = user; //ctx.state通常都从来放用户信息
+  } catch (err) {
+    ctx.throw(401, err.message);
+  }
+  await next();
+};
+
 router.get('/', getUser);
 
 router.post('/', createUser);
 
 router.get('/:id', getUserById);
 
-router.patch('/:id', updateUser); //partly update
+router.patch('/:id', auth, updateUser); //partly update
 
-router.delete('/:id', deleteUser);
+router.delete('/:id', auth, deleteUser);
 
 router.post('/login', login);
 
