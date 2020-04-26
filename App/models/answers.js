bfd193e02065b55d9b472aefd3db1652f8b02ae3
luckyleonard <dzhu31@hotmail.com>
2020-04-26 22:17:44 +1000

    ✨ (models) update models to store the date of each active

--- App/models/answers.js
+++ App/models/answers.js
@@ -2,17 +2,20 @@ const mongoose = require('mongoose');
 
 const { Schema, model } = mongoose;
 
-const answerSchema = new Schema({
-  __v: { type: Number, select: false },
-  content: { type: String, required: true },
-  answerer: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-    select: false,
+const answerSchema = new Schema(
+  {
+    __v: { type: Number, select: false },
+    content: { type: String, required: true },
+    answerer: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      select: false,
+    },
+    questionId: { type: String, required: true },
+    voteCount: { type: Number, required: true, default: 0 },
   },
-  questionId: { type: String, required: true },
-  voteCount: { type: Number, required: true, default: 0 },
-});
+  { timestamps: true }
+);
 //存储问题的id，和回答的人id
 module.exports = model('Answer', answerSchema);
--- App/models/questions.js
+++ App/models/questions.js
@@ -2,20 +2,23 @@ const mongoose = require('mongoose');
 
 const { Schema, model } = mongoose;
 
-const questionSchema = new Schema({
-  __v: { type: Number, select: false },
-  title: { type: String, required: true },
-  description: { type: String },
-  questioner: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-    select: false,
+const questionSchema = new Schema(
+  {
+    __v: { type: Number, select: false },
+    title: { type: String, required: true },
+    description: { type: String },
+    questioner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      select: false,
+    },
+    topics: {
+      type: [{ type: Schema.Types.ObjectId, ref: 'Topic' }],
+      select: false,
+    },
   },
-  topics: {
-    type: [{ type: Schema.Types.ObjectId, ref: 'Topic' }],
-    select: false,
-  },
-});
+  { timestamps: true }
+);
 //questioner 实现一对多关系，因为存在question表内
 module.exports = model('Question', questionSchema);
